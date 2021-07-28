fb79a01d552379cefcac4d9243876e364d50e872
Pallavi-GR <66360944+Pallavi-GR@users.noreply.github.com>
2021-07-28 07:50:03 +0200

    debugging 1

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ function timeDisplay() {
     var type = "Evening";
   }
 
-  if (20 >= hours && hours < 5) {
+  if (20 <= hours && hours < 5) {
     //Night
     document.write(
       '<body style="background: #0f2027; background: radial-gradient(circle at 10% 20%, rgb(90, 92, 106) 0%, rgb(32, 45, 58) 81.3%); opacity:0.8; \ncolor : white">'
