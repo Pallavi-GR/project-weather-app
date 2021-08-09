44a217b65943d3138e4756836892d2ac158357a8
Pallavi-GR <66360944+Pallavi-GR@users.noreply.github.com>
2021-08-09 15:47:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ timeDisplay();
 
 function displayForecast(response) {
   console.log(response.data);
-  console.log("Hello");
+  //console.log("Hello");
   let forecastElement = document.querySelector("#ff");
   let forecastHTML = `<div class = "row">`;
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
