4070f5bf055855b3365ce1af3f49711bb1c829a8
Pallavi-GR <66360944+Pallavi-GR@users.noreply.github.com>
2021-08-09 15:32:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ function searchPosition(event) {
 }
 
 function getForecast() {
-  console.log(coordinates);
+  //console.log(coordinates);
   let apiKey = "";
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   console.log(apiURL);
