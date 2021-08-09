bc8bd5072633efd501f3c5c68b691ea3994ce4c5
Pallavi-GR <66360944+Pallavi-GR@users.noreply.github.com>
2021-08-09 17:13:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ function handlePosition(position) {
   console.log(`Longitude: ${longitude}`);
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`;
   axios.get(`${url}&appid=${apiKey}`).then(showPosition);
+  //getForecast();
 }
 
 function showPosition(response) {
@@ -131,7 +132,6 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "";
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-
   console.log(apiURL);
   axios.get(apiURL).then(displayForecast);
 }
@@ -187,7 +187,7 @@ function timeDisplay() {
 timeDisplay();
 
 function displayForecast() {
-  //console.log(response.data);
+  console.log(response.data);
   let forecastElement = document.querySelector("#ff");
   let forecastHTML = `<div class = "row">`;
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
